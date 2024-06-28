b6fe29b00ab08b5c08641154918aa9350268e6b4
dunsinadesina <adesinajesudunsin@gmail.com>
2024-06-28 20:41:59 +0100

    Implement a class named Pricing from 3-currency.js

---
+++ 0x02-ES6_classes/4-main.js
@@ -0,0 +1,6 @@
+import Currency from './3-currency.js';
+import Pricing from './4-pricing.js';
+
+const p = new Pricing(100, new Currency("EUR", "Euro"))
+console.log(p);
+console.log(p.displayFullPrice());
