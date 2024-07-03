9e32494cd7d6c26d77a6956e550c184e69406af4
dunsinadesina <adesinajesudunsin@gmail.com>
2024-07-03 18:34:55 +0100

    task 4

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,9 +1,14 @@
 namespace Subjects {
-    export interface Teacher{
+    export interface Teacher {
         experienceTeachingC?: number;
     }
 
     export class Cpp extends Subject {
-        getRequirements
+        getRequirements(): string {
+            return `Here is the list of requirements for Cpp`;
+        }
+        getAvailableTeacher(): string {
+            return this.teacher.experienceTeachingC > 0 ? `Available teacher: ${this.teacher.firstName}` : 'No available teacher';
+        }
     }
 }
